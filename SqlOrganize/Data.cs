f1698c1f0141d8b2a9bebb5c3a098af8a558b9ab
ivancas84 <ivancas84@gmail.com>
2024-05-24 18:30:31 -0300

    Incorporar propiedad de uso opcional IsUpdated.

--- SqlOrganize/Data.cs
+++ SqlOrganize/Data.cs
@@ -8,10 +8,12 @@ namespace SqlOrganize
     /// </summary>
     public abstract class Data : INotifyPropertyChanged, IDataErrorInfo
     {
-        /// <summary>
-        /// Flag para indicar que debe ejecutarse la validacion
-        /// </summary>
-        public bool Validate = false;
+
+        /// <summary>Propiedad opcional para indicar que se esta actualizando</summary>
+        public bool _isUpdated = false;
+
+        /// <summary>Flag opcional para indicar que debe ejecutarse la validacion</summary>
+        public bool _Validate = false;
 
         /// <summary>
         /// Si se construye una instancia de data con valores por defecto, puede ser necesario acceder a la base de datos para definirlos.
@@ -22,7 +24,7 @@ namespace SqlOrganize
         {
             get
             {
-                if (!Validate)
+                if (!_Validate)
                     return "";
 
                 // If there's no error, empty string gets returned
@@ -62,10 +64,26 @@ namespace SqlOrganize
 
         public string Msg { get; set; } = "";
 
+        public bool IsUpdated
+        {
+            get { return _isUpdated; }
+            set
+            {
+                if(_isUpdated != value)
+                {
+                    _isUpdated = value;
+                    NotifyPropertyChanged("IsUpdated");
+                }
+            }
+        }
+
         public event PropertyChangedEventHandler? PropertyChanged;
 
         protected void NotifyPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] String propertyName = "")
         {
+            if(!propertyName.Equals(nameof(IsUpdated)))
+                IsUpdated = true;
+
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
